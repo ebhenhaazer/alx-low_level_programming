0ae1423704fab9bd905419854dce4c4e99ccb543
Elijah Mwathi <en.mwathy@gmail.com>
2023-02-21 00:18:04 -0800

    My first C project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print size of computer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("size of a char: %d byte(s)\n, sizeof(char));
+}
