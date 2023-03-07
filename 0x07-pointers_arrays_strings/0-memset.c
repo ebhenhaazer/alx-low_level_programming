14056656099c6e4450eac1293b49526a111c9a33
Elijah Mwathi <en.mwathy@gmail.com>
2023-03-06 16:22:31 -0800

    corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
+ * *_memset - set memory function
+ * @s: pointer to array
+ * @b: char type
+ * @n: unsigned int
+ * Return: s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int x;
 
 	for (x = 0; n > 0; x++)
 	{
