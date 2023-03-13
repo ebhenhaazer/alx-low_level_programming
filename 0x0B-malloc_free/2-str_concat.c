2a04f304a48068340b8ed93af76deb768052de44
Elijah Mwathi <en.mwathy@gmail.com>
2023-03-13 14:15:06 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,49 @@
-#include "main.h"
+#include "notrebloh.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
 
 /**
- * *str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to new space in memory or null
- **/
-/**
- * *str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to new space in memory or null
- **/
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k, e, coun;
-	int coun1 = 0;
-	int coun2 = 0;
-	char *q;
+	char *ptr;
+	int size1, size2;
 
 	if (s1 == NULL)
 	{
@@ -29,27 +53,16 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		coun1 = coun1 + 1;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		coun2 = coun2 + 1;
-	}
-	coun = coun1 + coun2;
-	q = malloc(sizeof(char) * coun + 1);
-	if (q == NULL)
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for (k = 0; s1[k] != '\0'; k++)
-	{
-		q[k] = s1[k];
-	}
-	for (k = k, e = 0; s2[e] != '\0'; e++, k++)
-	{
-		q[k] = s2[e];
-	}
-	q[k] = '\0';
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
