949332615ec7060d9f43894f1677699d68ee9f1d
Elijah Mwathi <en.mwathy@gmail.com>
2023-03-21 05:33:58 -0700

    corrections

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_realloc - reallocate memory size function
+ * @ptr: pointer to address of old memory location
+ * @old_size: unsigned int type of old memory size
+ * @new_size: unsigned int type for new memory size
+ * Return:  return pointer to array
+ */
+
+int main(void)
+{
+	int mul, num1, num2;
+	
+	if (num1 && num2 >= 0)
+	{
+		mul = num1 * num2;
+	}
+
+	else {
+		printf("Error");
+		return (98);
+	}
+	return (0);
+}
