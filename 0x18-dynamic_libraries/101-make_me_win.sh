b53c2f5fbad243b7a6bfb3e7b64f23ed462c4f3c
ebhenhaazer <en.mwathy@gmail.com>
2023-08-21 13:34:15 -0700

    changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://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@github.com/ebhenhaazer/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+wget -P /tmp https://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@github.com/ebhenhaazer/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
