31073dd88327f72601bad6b231e41bd10ea8c18a
ebhenhaazer <en.mwathy@gmail.com>
2023-09-11 14:36:06 -0700

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,17 +7,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned int index;
-	hash_node_t *temp;
+	hash_node_t *temp = NULL;
+
+	if (ht == NULL)
+		return;
 
 	for (index = 0; index < ht->size; index++)
 	{
-		while (ht->array[index])
+		if (ht->array[index] != NULL)
 		{
-			temp = ht->array[index]->next;
-			free(ht->array[index]->value);
-			free(ht->array[index]->key);
-			free(ht->array[index]);
-			ht->array[index] = temp;
+			while (ht->array[index])
+			{
+				temp = ht->array[index];
+				free(temp->value);
+				free(temp->key);
+				ht->array[index] = ht->array[index]->next;
+				free(temp);
+			}
 		}
 	}
 	free(ht->array);
