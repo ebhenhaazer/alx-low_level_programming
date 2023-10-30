a33c1d21a2c09a7fa38296a808309b85d56a87cc
ebhenhaazer <en.mwathy@gmail.com>
2023-10-29 20:07:14 -0700

    corrections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+""" Module that contains the island_perimeter function """
+
 
 def island_perimeter(grid):
-    for raw in range(len(grid)):
-        for col in range(len(grid[raw])):
-            print(grid[raw][col],end=' ')
-            if grid[raw][col] == 1:
-                print("number one", end="")
+    """ Function that returns the perimeter of island described in grid """
+    perimeter = 0
 
-                
-        print()
-"""
+    nrows = len(grid)
 
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
+    if grid != []:
+        ncolumns = len(grid[0])
 
-    print(island_perimeter(grid))
-"""
+    for a in range(nrows):
+        for b in range(ncolumns):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == nrows or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == ncolumns or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
